1c92e88c53b7ece53ce16f92d44f8e781b1eeb40
sarahelizabeth <smurray@thisismess.com>
2024-09-10 10:48:18 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button className='btn' onClick={() => document.getElementById('my_modal_5').showModal()}>
+          open modal
+        </button>
+        <dialog id='my_modal_5' className='modal'>
+          <div className='modal-box'>
+            <h3 className='font-bold text-lg'>Hello!</h3>
+            <p className='py-4'>Press ESC key or click the button below to close</p>
+            <div className='modal-action'>
+              <form method='dialog'>
+                {/* if there is a button in form, it will close the modal */}
+                <button className='btn'>Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+        <p className='text-red-800'>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App
