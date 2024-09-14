9007563556d12b13efccb87103473b81d569a557
sarahelizabeth <smurray@thisismess.com>
2024-09-13 20:12:45 -0500

    removed env var

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const API = axios.create({
   // baseURL: `http://localhost:8000`,
-  // baseURL: `https://api.sarahcodes.xyz`,
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: `https://api.sarahcodes.xyz`,
+  // baseURL: import.meta.env.VITE_API_URL
 });
